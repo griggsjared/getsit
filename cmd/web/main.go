3e2082e469e720c21ee0f234cbdc853c812204b1
Jared Griggs <griggsjared@gmail.com>
2024-11-02 20:34:13 -0600

    remove mongo db repostiory

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -10,8 +10,6 @@ import (
 	"github.com/gorilla/csrf"
 	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/joho/godotenv"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
 
 	"github.com/griggsjared/getsit/internal/repository"
 	"github.com/griggsjared/getsit/internal/service"
@@ -22,8 +20,6 @@ type app struct {
 	logger  *log.Logger
 }
 
-const database = "POSTGRES"
-
 func main() {
 	godotenv.Load()
 
@@ -31,42 +27,20 @@ func main() {
 
 	var r service.UrlEntryRepository
 
-	if database == "MONGO" {
-		dsn := os.Getenv("MONGODB_DSN")
-		if dsn == "" {
-			fmt.Println("MONGODB_DSN is not set")
-			os.Exit(1)
-		}
-
-		client, err := setupMongoDB(ctx, dsn)
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
-		defer func() {
-			if err = client.Disconnect(ctx); err != nil {
-				fmt.Println(err)
-			}
-		}()
-		r = repository.NewMongoDBUrlEntryRepository(client)
-
-	} else {
-
-		dsn := os.Getenv("POSTGRES_DSN")
-		if dsn == "" {
-			fmt.Println("POSTGRES_DSN is not set")
-			os.Exit(1)
-		}
-
-		db, err := pgxpool.New(ctx, dsn)
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
-		defer db.Close()
-
-		r = repository.NewPGXUrlEntryRepository(db)
+	dsn := os.Getenv("DATABASE_DSN")
+	if dsn == "" {
+		fmt.Println("DATABASE_DSN is not set")
+		os.Exit(1)
 	}
+
+	db, err := pgxpool.New(ctx, dsn)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer db.Close()
+
+	r = repository.NewPGXUrlEntryRepository(db)
 
 	logger := log.New(os.Stdout, "", log.LstdFlags)
 
@@ -114,22 +88,4 @@ func main() {
 	http.ListenAndServe(serverAddr, app.middlewareStack(mux, app.loggerMiddleware))
 
 	fmt.Println("Server started on", serverAddr)
-}
-
-// setupMongoDB will setup the connection to the MongoDB database
-func setupMongoDB(ctx context.Context, uri string) (*mongo.Client, error) {
-
-	opts := options.Client().ApplyURI(uri)
-
-	client, err := mongo.Connect(ctx, opts)
-	if err != nil {
-		return nil, err
-	}
-
-	err = client.Ping(ctx, nil)
-	if err != nil {
-		return nil, err
-	}
-
-	return client, nil
 }
